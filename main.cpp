41a81306c9d9553794fbfcf7d3d7753cc1788d99
kozolex <kozolex@gmail.com>
2018-01-17 11:39:10 +0100

    HSV / Segmentacja + Panel kontrolny

--- main.cpp
+++ main.cpp
@@ -1,31 +1,43 @@
 #include <iostream>
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
-
 using namespace cv;
 using namespace std;
-
 int main()
 {
     VideoCapture cap(0); //Klasa do przechwytywania wideo z plików wideo, kamer itp.
-    if ( !cap.isOpened() )  // jeżeli nie uzyskasz połączenia
+    if ( !cap.isOpened() )  // jeżeli nie uzyskasz połączenia to...
     {
         cout << "Nie moge polaczyc sie z kamera / wideo" << endl;
         return -1;
     }
-    Mat imgTmp;
-    cap.read(imgTmp);
+    int iLowH=0, iHighH=179, iLowS=0, iHighS=255, iLowV=0, iHighV=255;
+    namedWindow("Kontrola", CV_WINDOW_AUTOSIZE);
+    createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    createTrackbar("HighH", "Control", &iHighH, 179);
+    createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    createTrackbar("HighS", "Control", &iHighS, 255);
+    createTrackbar("LowV", "Control", &iLowV, 255);//Value (0 - 255)
+    createTrackbar("HighV", "Control", &iHighV, 255);
+
+    Mat imgOriginal;
+    Mat imgHSV;
+    Mat imgThresholded;
     while (true)
     {
-        Mat imgOriginal;
         bool bSuccess = cap.read(imgOriginal);
         if (!bSuccess)
         {
             cout << "Nie mozna odczytac strumienia " << endl;
             break;
         }
+        cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
+        inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded);
+
         imshow("Kamera RGB", imgOriginal);
-        if (waitKey(30) == 27) //czekaj 30ms na wciśnięcie esc jeżeli zostanie wciśnięty przerwij pętle
+        imshow("Kamera HSV", imgHSV);
+        imshow("Segmentacja - wynik", imgThresholded);
+        if (waitKey(30) == 27)
         {
             cout << "Klawisz esc - KONIEC" << endl;
             break;
